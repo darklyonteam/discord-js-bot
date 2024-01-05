f96fa01c5fe13f9e2f2d048e128090f9e88c7c5e
Dark Overlord <111354371+darklyonteam@users.noreply.github.com>
2024-01-05 19:49:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  OWNER_IDS: [""], // Bot owner ID's
-  SUPPORT_SERVER: "", // Your bot support server
+  OWNER_IDS: ["1153172663838195724"], // Bot owner ID's
+  SUPPORT_SERVER: "https://discord.gg/dDwA7wUX", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
-    DEFAULT_PREFIX: "!", // Default prefix for the bot
+    DEFAULT_PREFIX: "/", // Default prefix for the bot
   },
   INTERACTIONS: {
     SLASH: false, // Should the interactions be enabled
     CONTEXT: false, // Should contexts be enabled
     GLOBAL: false, // Should the interactions be registered globally
-    TEST_GUILD_ID: "xxxxxxxxxxx", // Guild ID where the interactions should be registered. [** Test you commands here first **]
+    TEST_GUILD_ID: "1153175815022989414", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
     BOT_EMBED: "#068ADD",
@@ -59,11 +59,11 @@ module.exports = {
     // Refer to https://github.com/freyacodes/Lavalink to host your own lavalink server
     LAVALINK_NODES: [
       {
-        host: "localhost",
-        port: 2333,
-        password: "youshallnotpass",
+        host: "lavalink.lexnet.cc",
+        port: 443,
+        password: "lexn3tl@val!k",
         id: "Local Node",
-        secure: false,
+        secure: true,
       },
     ],
   },
@@ -104,9 +104,9 @@ module.exports = {
 
   PRESENCE: {
     ENABLED: false, // Whether or not the bot should update its status
-    STATUS: "online", // The bot's status [online, idle, dnd, invisible]
-    TYPE: "WATCHING", // Status type for the bot [PLAYING | LISTENING | WATCHING | COMPETING]
-    MESSAGE: "{members} members in {servers} servers", // Your bot status message
+    STATUS: "ImSus!!", // The bot's status [online, idle, dnd, invisible]
+    TYPE: "COMPEING", // Status type for the bot [PLAYING | LISTENING | WATCHING | COMPETING]
+    MESSAGE: "{members} imposters in {servers} servers", // Your bot status message
   },
 
   STATS: {
